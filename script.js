d41761b79b023cec2b6bc01180ea61f4aecd3d8a
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-05 19:19:40 +0200

    Create plan for the game

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("test");
+/*
+Algorithm:
+1. Generate computer choice:
+  1a. Use random function
+  1b. Multiply outcome by 3 and round down
+  1c. Pick rock-paper-scissors based on numbers 0-1-2 
+2. Ask for human choice:
+  2a. Use prompt to ask for input
+  2b. Store input in a variable
+3. Pick winner of a round
+  3a. Compare computer choice with human choice
+  3b. Pick winner of a round based on rock-paper-scissors game rules
+4. Repeat steps 1-3 five times
+  4a. Create for loop to call above functions
+  4b. Store scores in variables for computer and human
+5. Present winner of the game
+  5a. Compare scores for computer and human
+  5b. Pick winner of the whole game
+*/
+
